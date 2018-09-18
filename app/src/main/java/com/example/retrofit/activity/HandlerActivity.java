a6a271d511897eb02502c3c59dbf1a16c80b42d1
axiang <axiang@hboxs.com>
2018-09-18 16:58:16 +0800

    添加了引导页和启动页

---
+++ app/src/main/java/com/example/retrofit/activity/HandlerActivity.java
@@ -0,0 +1,37 @@
+package com.example.retrofit.activity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.retrofit.R;
+
+public class HandlerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_handler);
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Intent intent = new Intent(HandlerActivity.this, MainActivity.class);
+            startActivity(intent);
+            //执行一次后销毁本页面
+            finish();
+        }
+
+    };
+
+}
